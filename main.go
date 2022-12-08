c46e90f7e5e20e4b1d1eda3a00a5df3e6810da5d
Michael Rybka <merybka@gmail.com>
2022-12-08 20:19:20 +0000

    Create fileserver

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	flag.Parse()
+	dir := flag.Arg(0)
+	acmeEmail := flag.Arg(1)
+	certDir := flag.Arg(2)
+	handler := &Handler{Dir: dir}
+	manager := autocert.Manager{
+		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache(certDir),
+		HostPolicy: func(_ context.Context, host string) error {
+			hosts, err := listHosts(dir)
+			if err != nil {
+				return err
+			}
+			for _, h := range hosts {
+				if h == host {
+					return nil
+				}
+			}
+			return fmt.Errorf("host %q not allowed", host)
+		},
+		Email: acmeEmail,
+	}
+	listener := manager.Listener()
+	err := http.Serve(listener, handler)
+	panic(err)
+}
+
+func listHosts(dir string) ([]string, error) {
+	hosts := []string{}
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return hosts, err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			hosts = append(hosts, file.Name())
+		}
+	}
+	return hosts, nil
+}
+
+type Handler struct {
+	Dir string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	host := r.Host
+	dir := filepath.Join(h.Dir, host)
+	http.FileServer(http.Dir(dir)).ServeHTTP(w, r)
+}
