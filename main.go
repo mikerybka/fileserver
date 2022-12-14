d30cce2be720da5d2784d9e2689110e8210e3c3d
Michael Rybka <merybka@gmail.com>
2022-12-13 22:05:23 -0500

    wip

--- main.go
+++ main.go
@@ -190,7 +190,7 @@ func (a *auth) signup(w http.ResponseWriter, r *http.Request) {
 			_, _ = w.Write([]byte("bad request: " + err.Error()))
 			return
 		}
-		fmt.Println(r.Form.Encode())
+		fmt.Println(r.PostForm.Encode())
 		user := r.FormValue("user")
 		if len(user) < 5 {
 			w.WriteHeader(http.StatusBadRequest)
